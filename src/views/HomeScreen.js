0c7877a0f2bedc258d267fbeb05d9f16047cf63e
Shusaku HARADA <shusaku.dev+atrassian@gmail.com>
2019-08-11 23:59:28 +0900

    WIP

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,26 +1,26 @@
-// import React, {Fragment} from 'react';
-// import {
-//     SafeAreaView,
-//     StyleSheet,
-//     ScrollView,
-//     View,
-//     Text,
-//     StatusBar,
-//     Modal,
-//     Button,
-//   } from 'react-native';
+import React, {Fragment} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Modal,
+    Button,
+  } from 'react-native';
   
-// export default class HomeScreen extends React.Component {
-//     static navigationOptions = {
-//       title: 'Welcome',
-//     };
-//     render() {
-//       const {navigate} = this.props.navigation;
-//       return (
-//         <Button
-//           title="Go to Jane's profile"
-//           onPress={() => navigate('Profile', {name: 'Jane'})}
-//         />
-//       );
-//     }
-//   }
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+      title: 'Welcome',
+    };
+    render() {
+      const {navigate} = this.props.navigation;
+      return (
+        <Button
+          title="Go to Jane's profile"
+          onPress={() => navigate('Profile', {name: 'Jane'})}
+        />
+      );
+    }
+  }
--- src/views/ProfileScreen.js
+++ src/views/ProfileScreen.js
@@ -1,5 +1,21 @@
 import React, {Fragment} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Modal,
+    Button,
+  } from 'react-native';
 
-// export default class ProfileScreen extends React.Component {
-    
-// }
+export default class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Profile</Text>
+            </View>
+        )
+    }
+}
