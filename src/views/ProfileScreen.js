4ef66db4f519dbdca71788980d292a85ffd5c0c4
Shusaku HARADA <shusaku.dev+atrassian@gmail.com>
2019-08-12 10:32:59 +0900

    WIP) Navigation

--- src/views/ProfileScreen.js
+++ src/views/ProfileScreen.js
@@ -12,9 +12,18 @@ import {
 
 export default class ProfileScreen extends React.Component {
     render() {
+        const {navigate} = this.props.navigation;
         return (
             <View>
                 <Text>Profile</Text>
+                <Button
+                title="Next"
+                onPress={() => this.props.navigation.push('Profile', {name: 'Next'})}
+                />
+                <Button
+                title="back"
+                onPress={() => this.props.navigation.goBack()}
+                />
             </View>
         )
     }
